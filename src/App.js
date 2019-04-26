148676c8816981b00331ab7c54666a6d8d21f838
Marissa <m4rsibar@gmail.com>
2019-04-26 10:46:29 -0400

    scroll component on all routes

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import Home from "./Home";
 import MyWork from "./MyWork";
 import About from "./About";
 import Music from "./Music";
+import Scroll from "./Scroll";
 
 class App extends Component {
   render() {
     return (
       <HashRouter>
         <Route path="/" render={() => <Music />} />
+        <Route path="/" render={() => <Scroll />} />
         <Route exact path="/" render={() => <Home />} />
         <Route path="/work" component={MyWork} />
         <Route path="/about" component={About} />
