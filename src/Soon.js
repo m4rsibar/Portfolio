c9c7bc82c5418e224d6fd69afb46578e97112acd
Marissa Smith <42990185+m4rsibar@users.noreply.github.com>
2019-12-14 15:02:02 -0500

    Create Soon.js

---
+++ src/Soon.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class Soon extends Component {
+  render() {
+    return (
+      <div>
+        <h1 style={{ color: "black" }}>...So roomy in here...</h1>{" "}
+        <h4 style={{ textAlign: "center", fontWeight: "lighter" }}>
+          coming soon
+        </h4>
+      </div>
+    );
+  }
+}
+
+export default Soon;
